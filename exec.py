7ca5abbe228e440198525fa5a09607fefd73a1b9
boorooksus <boorooksus@gmail.com>
2020-09-03 02:23:56 +0900

    readme update

--- exec.py
+++ exec.py
@@ -1,9 +1,6 @@
 import time, sys, base64;start = time.time();sys.stdout=open('output.out', 'w', encoding='utf8')
 
-i = 0
-while i < 10:
-	print(i)
-	i += 1
+x = input()
 print()
 print('Running time: ',end='')
 print(time.time() - start)
