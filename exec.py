34a68b05e8796eddaaa2ec54d1c7773b5da7061e
boorooksus <boorooksus@gmail.com>
2020-09-03 01:04:46 +0900

    .

--- exec.py
+++ exec.py
@@ -1,6 +1,9 @@
 import time, sys, base64;start = time.time();sys.stdout=open('output.out', 'w', encoding='utf8')
 
-print("가나다라마바사아자차카타파하")
+i = 0
+while i < 10:
+	print(i)
+	i += 1
 print()
 print('Running time: ',end='')
 print(time.time() - start)
