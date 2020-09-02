e4da858f48def79ce49ad5447f874df3f6c85937
boorooksus <boorooksus@gmail.com>
2020-09-02 14:49:18 +0900

    textarea 수정

--- exec.py
+++ exec.py
@@ -1,4 +1,4 @@
-import time, sys, base64;start = time.time();sys.stdout=open('output.out', 'w', encoding='utf8');print('안녕')
+import time, sys, base64;start = time.time();sys.stdout=open('output.out', 'w', encoding='utf8');print('가나다라마ㅏ사')
 print()
 print('Run time: ', time.time() - start)
 sys.stdout.close()
--- main.js
+++ main.js
@@ -26,27 +26,6 @@ app.use(compression());
 //app.use('/', indexRouter);
 
 app.get('/', (request, response) => {
-    var html = `     
-      <!DOCTYPE html>
-      <html>
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <link rel="stylesheet" href="/css/style2.css">
-              <title>create</title>
-          </head>
-  
-          <body>
-              <form action = "/create_process2" method="post">
-              
-              <div id="description">
-                  <textarea placeholder="내용" name="description"></textarea>
-                  <input type = "submit">
-              </div>
-              </form>
-          </body>
-      </html>
-  `;
   var index = template.index();
   response.send(index);
   });
