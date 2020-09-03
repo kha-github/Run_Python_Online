2ac3e415262cddc1f4ac9471bb0d3ef84c157bc0
boorooksus <boorooksus@gmail.com>
2020-09-03 09:29:20 +0900

    파일 정리

--- data/exec2.py
+++
@@ -1,6 +0,0 @@
-#-*-coding:utf-8-*-
-import time, sys, base64;start = time.time()
-result = '한글'
-print(base64.b64encode('가나다'.encode('utf-8')))
-print('한글')
-print(time.time() - start)
--- data/test.py
+++
@@ -1,6 +0,0 @@
-import base64
-import sys
-
-result = sys.argv[1]
-result = str(result)
-print(base64.b64encode(result.encode('utf-8')))
