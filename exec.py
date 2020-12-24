8896c5f67ca74ee196def7091098764b49b7d53d
boorooksus <boorooksus@gmail.com>
2020-12-24 13:02:30 +0900

    정리

--- exec.py
+++
@@ -1,9 +0,0 @@
-import time, sys, base64;start = time.time();sys.stdout=open('output.out', 'w', encoding='utf8')
-
-for i in range(100):
-	for j in range(i):
-		print('*',end='')
-	print();
-print()
-print('Running time: ',end='')
-print(time.time() - start)
